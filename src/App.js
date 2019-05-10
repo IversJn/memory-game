732204c0d217468e7ed044e23ac1a33a01da0c32
Jacob Ivers <iversjn@gmail.com>
2019-05-10 15:04:28 -0400

    Base Build Finished

--- src/App.js
+++ src/App.js
@@ -10,12 +10,49 @@ class App extends Component {
     friends,
     score: 0,
     highscore: 0,
+    clickedFriends: []
   };
 
-  // countClick = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-    
-  // };
+  countClick = id => {
+    let clickedFriends = this.state.clickedFriends;
+    if(clickedFriends.includes(id)){
+      if(this.state.score > this.state.highscore)
+      {
+        this.setState({highscore: this.state.score})
+      }
+      this.setState({ clickedFriends: [], score: 0});
+      return;
+
+    }else{
+      clickedFriends.push(id)
+
+      if(clickedFriends.length === 12){
+        this.setState({score: 12, clickedFriends: []});
+        console.log('You Win');
+        return;
+      }
+      this.setState({ friends, score: clickedFriends.length, clickedFriends });
+
+
+    for (let i = friends.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [friends[i], friends[j]] = [friends[j], friends[i]];
+    }
+    this.forceUpdate();
+    // const shuffledArray = this.shuffle(friends);
+    // this.setState = ({ friends: shuffledArray });
+  }
+
+  };
+
+
+  // shuffle = (tempArray) => {
+  //   for (let i = tempArray.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [tempArray[i], tempArray[j]] = [tempArray[j], tempArray[i]];
+  //   }
+  //   return tempArray;
+  // }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -24,10 +61,9 @@ class App extends Component {
         <Heading score={this.state.score} highscore={this.state.highscore}></Heading>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            countClick={this.countClick}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
           />
         ))}
